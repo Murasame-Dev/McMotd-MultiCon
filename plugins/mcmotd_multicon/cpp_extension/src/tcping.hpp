721d46f4be80416f7e7ba82ddd43be86b49c16e2
quqiOnfree <quqiOnfree@outlook.com>
2025-12-21 12:22:31 +0800

    修复tcping超时问题 (#2)
    * 修复tcping超时问题
    
    * delete
    
    * format
    
    * Update plugins/mcmotd_multicon/cpp_extension/CMakeLists.txt

--- plugins/mcmotd_multicon/cpp_extension/src/tcping.hpp
+++ plugins/mcmotd_multicon/cpp_extension/src/tcping.hpp
@@ -12,33 +12,29 @@
 
 namespace net {
 
-asio::awaitable<std::expected<std::chrono::steady_clock::duration, std::string>>
-async_tcping(std::string_view host, std::uint16_t port) {
+asio::awaitable<std::chrono::milliseconds>
+async_tcping(std::string_view host, std::uint16_t port,
+             const std::chrono::steady_clock::duration &timeout) {
   using namespace asio::experimental::awaitable_operators;
 
   auto executor = co_await asio::this_coro::executor;
   asio::ip::tcp::resolver resolver(executor);
   asio::ip::tcp::socket socket(executor);
 
-  try {
-    auto port_str = std::to_string(port);
-    auto endpoint = *resolver.resolve(host, port_str).begin();
-    auto start = std::chrono::steady_clock::now();
-    co_await socket.async_connect(endpoint, asio::use_awaitable);
-    auto end = std::chrono::steady_clock::now();
-    std::error_code ec;
-    socket.close(ec);
-    co_return (end - start);
-
-  } catch (const std::system_error &e) {
-    co_return std::unexpected(e.code().message());
-
-  } catch (const std::exception &e) {
-    co_return std::unexpected(std::string(e.what()));
-
-  } catch (...) {
-    co_return std::unexpected(std::string());
+  auto port_str = std::to_string(port);
+  auto endpoint = *resolver.resolve(host, port_str).begin();
+  asio::steady_timer timer(executor);
+  timer.expires_after(timeout);
+  auto start = std::chrono::steady_clock::now();
+  auto res = co_await (socket.async_connect(endpoint, asio::use_awaitable) ||
+                       timer.async_wait(asio::use_awaitable));
+  auto end = std::chrono::steady_clock::now();
+  if (res.index()) {
+    throw std::system_error(std::make_error_code(std::errc::timed_out));
   }
+  std::error_code ec;
+  socket.close(ec);
+  co_return std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
 }
 
 } // namespace net
